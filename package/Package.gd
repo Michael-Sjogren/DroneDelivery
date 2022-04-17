4ea97bed899c6369ab69323a718af25e5cdcd0c2
michael <carl.michael.sjogren@gmail.com>
2022-04-17 11:50:34 +0200

    break sfx for packages

--- package/Package.gd
+++ package/Package.gd
@@ -3,6 +3,7 @@ export var max_health:int = 10
 var health:int
 var is_connected_to_drone := false
 var is_invulnerable = true
+var is_destroyed = false
 onready var package_radius:float = $Radius.shape.radius
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,7 +11,9 @@ func _ready():
 	health = max_health
 
 func destroy():
+	is_destroyed = true
 	Globals.emit_signal("package_destroyed", max_health)
+	$BreakSFX.pitch_scale = .6
 	$BreakSFX.play()
 	$PackageModel.visible = false
 	mode = RigidBody.MODE_STATIC
@@ -19,16 +22,18 @@ func destroy():
 	queue_free()
 
 func _physics_process(delta):
-	if health <= 0:
+	if health <= 0 and not is_destroyed:
 		destroy()
 
 
 func set_is_connected_to_drone(_is_connected:bool):
-	if _is_connected:
-		$PickupSFX.play()
+	if is_connected_to_drone:
+		is_invulnerable = true
+		$InvunrabillityTimer.start()
+	else:
+		$PickupSFX.play()		
 	contact_monitor = true
-	is_invulnerable = true
-	$InvunrabillityTimer.start()
+
 	#contact_monitor = !_is_connected
 	is_connected_to_drone = _is_connected
 
@@ -36,9 +41,11 @@ func _on_Package_body_entered(body):
 	if not is_invulnerable:
 		if not body.is_in_group("DropZone"):
 			health = clamp(health - 1 , 0 , max_health)
-			is_invulnerable = true
-			$InvunrabillityTimer.start()
-			print_debug("package hp: " , health)
+			if health > 0:
+				is_invulnerable = true
+				$InvunrabillityTimer.start()
+				$BreakSFX.pitch_scale = 2
+				$BreakSFX.play()
 
 func _on_InvunrabillityTimer_timeout():
 	is_invulnerable = false
